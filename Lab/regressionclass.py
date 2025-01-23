1ffbdcf1f70323d6d68d8ae88ecf544949e87379
zoneme99 <jonatancervall@gmail.com>
2025-01-23 15:56:54 +0100

    only function for significance is left for grade G

--- Lab/regressionclass.py
+++ Lab/regressionclass.py
@@ -5,11 +5,15 @@ import pandas as pd
 df = pd.read_csv("Lab/Small-diameter-flow.csv", index_col=0)
 
 class LinearRegression:
-    def __init__(self, matrix):
+    def __init__(self, matrix, yvariable):
         if isinstance(matrix, pd.DataFrame):
             (n, d) = matrix.shape
             self._n = n
             self._d = d
+            self._Y = matrix[yvariable]
+            self._X = matrix.drop(yvariable, axis=1)
+            self._X.insert(0, "bias", np.ones(self._X.shape[0]))
+            self._B = np.linalg.pinv(self._X.T @ self._X) @ self._X.T @ self._Y
         else:
             raise TypeError("Needs to be a Dataframe")
 
@@ -21,9 +25,46 @@ class LinearRegression:
     def d(self):
         return self._d
     
+    @property
+    def Y(self):
+        return self._Y.to_numpy()
+    
+    @property
+    def X(self):
+        return self._X.to_numpy()
+    
+    @property
+    def B(self):
+        return self._B.to_numpy()
+    
+    @property
+    def SSE(self):
+        return np.sum(np.square(self.Y - self.X @ self.B))
+    
+    @property
+    def var(self):
+        return self.SSE()/(self.n-self.d-1)
+    
+    @property
+    def std(self):
+        return np.sqrt(self.var())
+    
+    @property
+    def SSR(self):
+        return (self.n*np.sum(self.B*(self.X.T @ self.Y)) - (np.square(np.sum(self.Y))))/self.n
+    
+    @property
+    def SST(self):
+        return (self.n*np.sum(np.square(self.Y)) - np.square(np.sum(self.Y)))/self.n
+    
+    @property
+    def R2(self):
+        return self.SSR / self.SST
+    
+    
 
-test = LinearRegression(df)
+test = LinearRegression(df,"Flow")
 
-print(test.n,test.d)
+print(test.R2)
     
         
--- Lab/test.py
+++ Lab/test.py
@@ -1,5 +1,12 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("Lab/Small-diameter-flow.csv", index_col=0)
 #print(type(df))
-print(df.shape)
+X = df.drop('Flow', axis=1)
+Y = df['Flow']
+
+
+X.insert(0, "1", np.ones(X.shape[0]))
+b = np.linalg.pinv(X.T @ X) @ X.T @ Y
+print(b)
